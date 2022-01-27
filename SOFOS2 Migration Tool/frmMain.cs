9ff09242a9339d138bcaa50e9057100be31e9a8c
Lenard Aquino <lenardaquino@gmail.com>
2022-01-27 17:09:29 +0800

    Adjustment - :hammer: Adjusment in Main Form

--- SOFOS2 Migration Tool/frmMain.cs
+++ SOFOS2 Migration Tool/frmMain.cs
@@ -49,18 +49,33 @@ namespace SOFOS2_Migration_Tool
             if (UserConfirmation(ProcessEnum.Migrate, ModuleEnum.Payment))
                 return;
 
-            PaymentComputeController pcc = new PaymentComputeController();
-            var invoicelist = pcc.GetInvoice("CI"); 
-            //CollectionReceiptController crc = new CollectionReceiptController();
-            //var crheader = crc.GetCollectionReceiptHeader("2021-05-31", "OR");
-            //var crdetail = crc.GetCollectionReceiptDetail("2021-05-31", "OR");
-            //crc.InsertCR(crheader, crdetail);
+            try
+            { 
+                //PaymentComputeController pcc = new PaymentComputeController();
+                //var invoicelist = pcc.GetInvoice("CI"); 
 
-            //JournalVoucherController jvc = new JournalVoucherController();
-            //var jvheader = jvc.GetJournalVoucherHeader("2021-03-31","JV");
-            //var jvdetail = jvc.GetJournalVoucherDetail("2021-03-31", "JV");
+                CollectionReceiptController crc = new CollectionReceiptController();
+                var crheader = crc.GetCollectionReceiptHeader(date, "OR");
+                var crdetail = crc.GetCollectionReceiptDetail(date, "OR");
+                if (crheader.Count > 0)
+                    crc.InsertCR(crheader, crdetail);
 
-            pcbPayment.BackgroundImage = checkedImage;
+                //JournalVoucherController jvc = new JournalVoucherController();
+                //var jvheader = jvc.GetJournalVoucherHeader("2021-03-31","JV");
+                //var jvdetail = jvc.GetJournalVoucherDetail("2021-03-31", "JV");
+
+                string message = string.Format(@"No transactions found in SOFOS1 - Payment module (Collection Receipt) dated : {0}.", date);
+                if (crheader.Count > 0)
+                    message = string.Format(@" ({0}) Collection Receipt transactions was transfered successfully.", crheader.Count);
+
+                MessageBox.Show(message);
+
+                pcbPayment.BackgroundImage = checkedImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Error : {0}", ex.Message));
+            }
         }
 
         private void btnPurchasing_Click(object sender, EventArgs e)
@@ -69,24 +84,37 @@ namespace SOFOS2_Migration_Tool
             if (UserConfirmation(ProcessEnum.Migrate, ModuleEnum.Purchasing))
                 return;
 
-            #region Purchasing Module
-            //PurchaseRequestController test2 = new PurchaseRequestController();
+            try
+            { 
+                #region Purchasing Module
+                //PurchaseRequestController test2 = new PurchaseRequestController();
 
-            ReceiveFromVendorController receiveFromVendorController = new ReceiveFromVendorController();
-            var receiveFromVendorHeader = receiveFromVendorController.GetRVHeader(date);
-            var receiveFromVendorDetails = receiveFromVendorController.GetRVItem(date);
-            if (receiveFromVendorHeader.Count > 0)
-                receiveFromVendorController.InsertRV(receiveFromVendorHeader, receiveFromVendorDetails);
+                ReceiveFromVendorController receiveFromVendorController = new ReceiveFromVendorController();
+                var receiveFromVendorHeader = receiveFromVendorController.GetRVHeader(date);
+                var receiveFromVendorDetails = receiveFromVendorController.GetRVItem(date);
+                if (receiveFromVendorHeader.Count > 0)
+                    receiveFromVendorController.InsertRV(receiveFromVendorHeader, receiveFromVendorDetails);
 
-            ReturnGoodsController returnGoodsController = new ReturnGoodsController();
-            var returnGoodsHeader = returnGoodsController.GetRGHeader(date);
-            var returnGoodsDetails = returnGoodsController.GetRGItem(date);
-            if (returnGoodsHeader.Count > 0)
-                returnGoodsController.InsertReturnGoods(returnGoodsHeader, returnGoodsDetails);
+                ReturnGoodsController returnGoodsController = new ReturnGoodsController();
+                var returnGoodsHeader = returnGoodsController.GetRGHeader(date);
+                var returnGoodsDetails = returnGoodsController.GetRGItem(date);
+                if (returnGoodsHeader.Count > 0)
+                    returnGoodsController.InsertReturnGoods(returnGoodsHeader, returnGoodsDetails);
 
-            #endregion Purchasing Module
+                #endregion Purchasing Module
 
-            pcbPurchasing.BackgroundImage = checkedImage;
+                string message = string.Format(@"No transactions found in SOFOS1 - Purchasing module(Receive From Vendor  and Receive Goods) dated : {0}.", date);
+                if (receiveFromVendorHeader.Count + returnGoodsHeader.Count > 0)
+                    message = string.Format(@" ({0}) Receive From Vendor  and ({1}) Receive Goods transactions was transfered successfully.", receiveFromVendorHeader.Count, returnGoodsHeader.Count);
+
+                MessageBox.Show(message);
+
+                pcbPurchasing.BackgroundImage = checkedImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Error : {0}", ex.Message));
+            }
 
         }
 
@@ -96,27 +124,39 @@ namespace SOFOS2_Migration_Tool
         {
             if (UserConfirmation(ProcessEnum.Migrate, ModuleEnum.Sales))
                 return;
-            
-            #region Sales Module
 
-            SalesController salesController = new SalesController();
-            var salesHeader = salesController.GetSalesHeader(date);
-            var salesDetails = salesController.GetSalesItems(date);
-            var salesPayment = salesController.GetSalesPayment(date);
-            if (salesHeader.Count > 0)
-                salesController.InsertSales(salesHeader, salesDetails, salesPayment);
+            try
+            {
+                #region Sales Module
+
+                SalesController salesController = new SalesController();
+                var salesHeader = salesController.GetSalesHeader(date);
+                var salesDetails = salesController.GetSalesItems(date);
+                var salesPayment = salesController.GetSalesPayment(date);
+                if (salesHeader.Count > 0)
+                    salesController.InsertSales(salesHeader, salesDetails, salesPayment);
 
 
-            ReturnFromCustomerController returnFromCustomerController = new ReturnFromCustomerController();
-            var returnFromCustomerHeader = returnFromCustomerController.GetReturnFromCustomerHeader(date);
-            var returnFromCustomerDetails = returnFromCustomerController.GetReturnFromCustomerItems(date);
-            if (returnFromCustomerHeader.Count > 0)
-                returnFromCustomerController.InsertReturnFromCustomer(returnFromCustomerHeader, returnFromCustomerDetails);
-            
-            #endregion Sales Module
+                ReturnFromCustomerController returnFromCustomerController = new ReturnFromCustomerController();
+                var returnFromCustomerHeader = returnFromCustomerController.GetReturnFromCustomerHeader(date);
+                var returnFromCustomerDetails = returnFromCustomerController.GetReturnFromCustomerItems(date);
+                if (returnFromCustomerHeader.Count > 0)
+                    returnFromCustomerController.InsertReturnFromCustomer(returnFromCustomerHeader, returnFromCustomerDetails);
 
-            pcbSales.BackgroundImage = checkedImage;
+                #endregion Sales Module
 
+                string message = string.Format(@"No transactions found in SOFOS1 - Point of Sale module (Sales and Return from Customer) dated : {0}.", date);
+                if (salesHeader.Count + returnFromCustomerHeader.Count > 0)
+                    message = string.Format(@" ({0}) Sales and ({1}) Return from Customer transactions was transfered successfully.", salesHeader.Count, returnFromCustomerHeader.Count);
+
+                MessageBox.Show(message);
+
+                pcbSales.BackgroundImage = checkedImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Error : {0}",ex.Message));
+            }
         }
 
         
@@ -124,31 +164,41 @@ namespace SOFOS2_Migration_Tool
         {
             if (UserConfirmation(ProcessEnum.Migrate, ModuleEnum.Inventory))
                 return;
+            try
+            {
+                #region Inventory Module
 
-            #region Inventory Module
+                GoodsReceiptController goodsReceiptController = new GoodsReceiptController();
+                var goodsReceiptdata = goodsReceiptController.GetGoodsReceiptHeader(date);
+                var goodsReceiptdetail = goodsReceiptController.GetGoodsReceiptItems(date);
+                if (goodsReceiptdata.Count > 0)
+                    goodsReceiptController.InsertGoodsReceipt(goodsReceiptdata, goodsReceiptdetail);
 
-            GoodsReceiptController goodsReceiptController = new GoodsReceiptController();
-            var goodsReceiptdata = goodsReceiptController.GetGoodsReceiptHeader(date);
-            var goodsReceiptdetail = goodsReceiptController.GetGoodsReceiptItems(date);
-            if (goodsReceiptdata.Count > 0)
-                goodsReceiptController.InsertGoodsReceipt(goodsReceiptdata, goodsReceiptdetail);
+                GoodsIssuanceController goodsIssuanceController = new GoodsIssuanceController();
+                var goodsIssuancedata = goodsIssuanceController.GetGoodsIssuanceHeader(date);
+                var goodsIssuancedetail = goodsIssuanceController.GetGoodsIssuanceItems(date);
+                if (goodsIssuancedata.Count > 0)
+                    goodsIssuanceController.InsertGoodsIssuance(goodsIssuancedata, goodsIssuancedetail);
 
-            GoodsIssuanceController goodsIssuanceController = new GoodsIssuanceController();
-            var goodsIssuancedata = goodsIssuanceController.GetGoodsIssuanceHeader(date);
-            var goodsIssuancedetail = goodsIssuanceController.GetGoodsIssuanceItems(date);
-            if (goodsIssuancedata.Count > 0)
-                goodsIssuanceController.InsertGoodsIssuance(goodsIssuancedata, goodsIssuancedetail);
+                AdjustmentController adjustmentController = new AdjustmentController();
+                var adjustmentData = adjustmentController.GetAdjustmentHeader(date);
+                var adjustmentDetail = adjustmentController.GetAdjustmentItems(date);
+                if (adjustmentData.Count > 0)
+                    adjustmentController.InsertAdjustment(adjustmentData, adjustmentDetail);
 
-            AdjustmentController adjustmentController = new AdjustmentController();
-            var adjustmentData = adjustmentController.GetAdjustmentHeader(date);
-            var adjustmentDetail = adjustmentController.GetAdjustmentItems(date);
-            if (adjustmentData.Count > 0)
-                adjustmentController.InsertAdjustment(adjustmentData, adjustmentDetail);
+                #endregion Inventory Module
 
-            #endregion Inventory Module
+                string message = string.Format(@"No transactions found in SOFOS1 - Inventory module (Goods Receipt, Goods Issuances  and Inventory Adjustments ) dated : {0}.", date);
+                if (goodsReceiptdata.Count + goodsIssuancedata.Count + adjustmentData.Count > 0)
+                    message = string.Format(@" ({0}) Goods Receipt, {1} Goods Issuances and ({2}) Inventory Adjustments transactions was transfered successfully.", goodsReceiptdata.Count, goodsIssuancedata.Count, adjustmentData.Count);
 
-            pcbInventory.BackgroundImage = checkedImage;
-
+                MessageBox.Show(message);
+                pcbInventory.BackgroundImage = checkedImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Error : {0}", ex.Message));
+            }
         }
 
         private Image GetImageByResult(string salesModule)
@@ -197,34 +247,67 @@ namespace SOFOS2_Migration_Tool
         {
             if (UserConfirmation(ProcessEnum.Recompute, ModuleEnum.Inventory))
                 return;
+            try
+            { 
+                #region Recompute Value and Quantity
+                RecomputeController recompute = new RecomputeController();
+                var trans = recompute.GetTransactions(date);
+                if(trans.Count > 0)
+                    recompute.UpdateRunningQuantityValueCost(trans);
+                #endregion
 
-            #region Recompute Value and Quantity
-            RecomputeController recompute = new RecomputeController();
-            var trans = recompute.GetTransactions(date);
-            recompute.UpdateRunningQuantityValueCost(trans);
-            #endregion
+                string message = string.Format(@"No transactions(Purchasing, Inventory or Sales module) found in SOFOS2 dated : {0}.", date);
+                if (trans.Count > 0)
+                    message = string.Format(@" ({0}) transactions in 'Purchasing, Inventory or Sales module' was recomputed successfully.", trans.Count);
 
-            pcbRecomputeInventory.BackgroundImage = checkedImage;
+                MessageBox.Show(message);
+
+                pcbRecomputeInventory.BackgroundImage = checkedImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Error : {0}", ex.Message));
+            }
         }
 
         private void dtpDateParam_ValueChanged(object sender, EventArgs e)
         {
             date = dtpDateParam.Value.ToString("yyyy-MM-dd");
+            pcbPurchasing.BackgroundImage = null;
+            pcbCreditLimit.BackgroundImage = null;
+            pcbInventory.BackgroundImage = null;
+            pcbPayment.BackgroundImage = null;
+            pcbRecomputeInventory.BackgroundImage = null;
+            pcbRecomputePayment.BackgroundImage = null;
+            pcbRecomputeSalesCreditLimit.BackgroundImage = null;
+            pcbSales.BackgroundImage = null;
         }
 
         private void btnRecomputeSalesCreditLimit_Click(object sender, EventArgs e)
         {
             if (UserConfirmation(ProcessEnum.Recompute, ModuleEnum.SalesCreditLimit))
                 return;
+            try
+            { 
+                #region Recompute creditlimit amount
+                ReComputeSalesCreditController reComputeSalesCreditController = new ReComputeSalesCreditController();
+                var reComputeSalesCredit = reComputeSalesCreditController.GetSalesAndReturnFromCustomerTransactions(date);
+                if (reComputeSalesCredit.Count > 0)
+                    reComputeSalesCreditController.UpdateChargeAmount(reComputeSalesCredit);
+                #endregion
 
-            #region Recompute creditlimit amount
-            ReComputeSalesCreditController reComputeSalesCreditController = new ReComputeSalesCreditController();
-            var reComputeSalesCredit = reComputeSalesCreditController.GetSalesAndReturnFromCustomerTransactions(date);
-            if (reComputeSalesCredit.Count > 0)
-                reComputeSalesCreditController.UpdateChargeAmount(reComputeSalesCredit);
-            #endregion
+                string message = string.Format(@"No transactions in Sales module (CI,CT,CO and AP) found in SOFOS2 dated : {0}.", date);
+                if (reComputeSalesCredit.Count > 0)
+                    message = string.Format(@" ({0}) sales transactions with credit was recomputed successfully.", reComputeSalesCredit.Count);
 
-            pcbRecomputeSalesCreditLimit.BackgroundImage = checkedImage;
+                MessageBox.Show(message);
+
+                pcbRecomputeSalesCreditLimit.BackgroundImage = checkedImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Error : {0}", ex.Message));
+            }
         }
 
         private void btnCreditLimit_Click(object sender, EventArgs e)
@@ -232,16 +315,29 @@ namespace SOFOS2_Migration_Tool
             if (UserConfirmation(ProcessEnum.Migrate, ModuleEnum.SalesCreditLimit))
                 return;
 
-            #region Credit Limit
+            try
+            { 
+                #region Credit Limit
 
-            AccountCreditLimitController accountCreditLimitController = new AccountCreditLimitController();
-            var accountCreditLimitData = accountCreditLimitController.GetAccountCreditLimits(date);
-            if (accountCreditLimitData.Count > 0)
-                accountCreditLimitController.InsertAccountCreditLimits(accountCreditLimitData);
+                AccountCreditLimitController accountCreditLimitController = new AccountCreditLimitController();
+                var accountCreditLimitData = accountCreditLimitController.GetAccountCreditLimits(date);
+                if (accountCreditLimitData.Count > 0)
+                    accountCreditLimitController.InsertAccountCreditLimits(accountCreditLimitData);
 
-            #endregion Credit Limit
+                #endregion Credit Limit
 
-            pcbCreditLimit.BackgroundImage = checkedImage;
+                string message = string.Format(@"No data found in SOFOS1 - Accounting module (Credit Limit) dated : {0}.", date);
+                if (accountCreditLimitData.Count > 0)
+                    message = string.Format(@" ({0}) Credit Limit transactions was transfered successfully.", accountCreditLimitData.Count);
+
+                MessageBox.Show(message);
+
+                pcbCreditLimit.BackgroundImage = checkedImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Error : {0}", ex.Message));
+            }
         }
     }
 }
