019b784baed52b1f9282fb5f136d160d5888916b
Angelito De Sagun <desagunangelito@gmail.com>
2022-05-11 14:47:25 +0800

    remove remaining GO in sales

--- SOFOS2 Migration Tool/Service/SalesQuery.cs
+++ SOFOS2 Migration Tool/Service/SalesQuery.cs
@@ -91,7 +91,7 @@ namespace SOFOS2_Migration_Tool.Service
                                     INNER JOIN stocks s ON i.idstock = s.idstock
                                     LEFT JOIN files f ON l.idfile = f.idfile
                                     LEFT JOIN coa c ON l.idaccount = c.idaccount
-                                    where LEFT(l.reference, 2) IN ('SI','CI','CO','AP','CT','EC','FS','GO','RT','CP','SB','PI','CB','BT','CS','RT','CL', 'CG','PI')
+                                    where LEFT(l.reference, 2) IN ('SI','CI','CO','AP','CT','EC','FS','RT','CP','SB','PI','CB','BT','CS','RT','CL', 'CG')
                                     AND date(l.date) = @date
                                     GROUP BY l.reference
                                     ORDER BY l.date ASC;
@@ -154,7 +154,7 @@ namespace SOFOS2_Migration_Tool.Service
                                     INNER JOIN pcosting p ON i.idstock = p.idstock AND i.unit = p.unit
                                     WHERE
                                     /*left(i.reference, 2) = @transType AND date(l.date) = @date*/
-                                    LEFT(i.reference, 2) IN ('SI','CI','CO','AP','CT','EC','FS','GO','RT','CP','SB','PI','CB','BT','CS','RT','CL','CG','PI')
+                                    LEFT(i.reference, 2) IN ('SI','CI','CO','AP','CT','EC','FS','RT','CP','SB','PI','CB','BT','CS','RT','CL', 'CG')
                                     AND date(l.date) = @date
                                     GROUP BY i.reference, i.idstock, i.unit
                                     ORDER BY l.reference ASC;
@@ -178,7 +178,7 @@ namespace SOFOS2_Migration_Tool.Service
                                     extracted AS 'Extracted',
                                     0 AS 'OrDetailNum'
                                      FROM transactionpayments
-                                    WHERE LEFT(reference, 2) IN ('SI','CI','CO','AP','CT','EC','FS','GO','RT','CP','SB','PI','CB','BT','CS','RT','CL', 'CG','PI') AND date(date) = @date;
+                                    WHERE LEFT(reference, 2) IN ('SI','CI','CO','AP','CT','EC','FS','RT','CP','SB','PI','CB','BT','CS','RT','CL', 'CG') AND date(date) = @date;
                                      ");
                     break;
                 default:
